7952e8e22b9ba2990ce99a11a39aac1e9406c200
Romain <thegrandtour29@gmail.com>
2020-12-09 17:06:31 +0100

    Base de données

--- database.php
+++ database.php
@@ -7,7 +7,6 @@
   try {
       $db =new PDO("mysql:host=" . HOST .";dbname=" .DB_NAME,USER,PASS);
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo"hihi";}
    catch(PDOException $e) {
       echo $e;
   }
